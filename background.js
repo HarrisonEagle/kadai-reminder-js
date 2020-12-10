1d72ae60a0a9a78e727919ea87d0944101d58bbc
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2020-12-10 19:07:03 +0900

    Fix Frontend and notificator

--- background.js
+++ background.js
@@ -1,4 +1,53 @@
-function getinf() {
-
+function notifyMe(title,msg,url) {
+ if (Notification.permission !== 'granted'){
+  Notification.requestPermission();
+}else {
+  var notification = new Notification(title, {
+   body: msg,
+  });
+  notification.onclick = function() {
+    window.open(url, '_blank');
+  };
+ }
 }
-//ログインしてません
+
+function getinf(){
+  chrome.storage.local.get(['wasedaid', 'password'], function (results) {
+    //Communication
+    $("#tab2").empty();
+    $.ajax({
+        url: "https://kadai-reminder-server.herokuapp.com/getinf/"+results.wasedaid,
+        type: 'GET',
+        success: function(json) {
+          getresult(results.wasedaid);
+        }
+    });
+});
+}
+
+
+function getresult(username){
+  $.ajax({
+      url: "https://kadai-reminder-server.herokuapp.com/result/"+username,
+      type: 'GET',
+      success: function(json) {
+        const obj = JSON.parse(json);
+        if(Array.isArray(obj)){
+          chrome.storage.local.set({"kadaidata":json},function(){
+            for(var i=0;i<obj.length;i++) {
+              $("#tab2").append(`<div class=\"listchild\"><a href=\"${obj[i].url}\" class=\"cname\" target="_blank">${obj[i].name}</a><p class=\"cdate\">${obj[i].deadline}</p></div>`);
+              notifyMe(obj[i].name,`活動は${obj[i].deadline}が期限です`,obj[i].url);
+            }
+          });
+        }else{
+          if(obj.name == undefined){
+            setTimeout(function(){ getresult(username);}, 10000);
+          }else{
+            alert("ログインに失敗しました");
+          }
+
+        }
+
+      }
+  });
+}
--- popup.js
+++ popup.js
@@ -8,35 +8,43 @@ chrome.storage.local.get('password', function (results) {
   $("#password").val(CryptoJS.AES.decrypt(results.password, "Passphrase").toString(CryptoJS.enc.Utf8));
 });
 
+chrome.storage.local.get('kadaidata',function(json){
+  const obj = JSON.parse(json.kadaidata);
+  for(var i=0;i<obj.length;i++) {
+    $("#tab2").append(`<div class=\"listchild\"><a href=\"${obj[i].url}\" class=\"cname\" target="_blank">${obj[i].name}</a><p class=\"cdate\">${obj[i].deadline}</p></div>`);
+  }
+});
+
   //Button
   var reload = document.getElementById("reload");
   reload.addEventListener("click", function(){
-    chrome.storage.local.get(['wasedaid', 'password'], function (results) {
-      //Communication
-      var form = $('<form/>', {action: "https://kadai-reminder-server.herokuapp.com/api", method: 'post'})
-      .append($('<input/>', {name: 'wasedaid', value: CryptoJS.AES.encrypt($("#wasedaid").val(), "Passphrase").toString()}))
-      .append($('<input/>', {name: "password", type:"password",value: results.password}))
-      $.ajaxSetup({
-        headers: {
-          '  X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-     }
-   });
-      $.ajax({
-	        url: "https://kadai-reminder-server.herokuapp.com/api",
-	        type: 'POST',
-          data :form.serialize(),
-	        success: function(data) {
-	            alert(data);
-	        }
-	    });
-
-  });
+    getinf();
   });
 
   var save = document.getElementById("save");
   save.addEventListener("click", function(){
-    alert(CryptoJS.AES.encrypt($("#password").val(), "Passphrase").toString());
-    chrome.storage.local.set({wasedaid:$("#wasedaid").val(),password:CryptoJS.AES.encrypt($("#password").val(), "Passphrase").toString()});
+    chrome.storage.local.set({wasedaid:$("#wasedaid").val(),password:CryptoJS.AES.encrypt($("#password").val(), "Passphrase").toString()},function(){
+      chrome.storage.local.get(['wasedaid', 'password'], function (results) {
+        //Communication
+        var form = $('<form/>', {action: "https://kadai-reminder-server.herokuapp.com/upuser", method: 'post'})
+        .append($('<input/>', {name: 'wasedaid', value: CryptoJS.AES.encrypt($("#wasedaid").val(), "Passphrase").toString()}))
+        .append($('<input/>', {name: "password", type:"password",value: results.password}))
+        $.ajaxSetup({
+          headers: {
+            '  X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+       }
+     });
+        $.ajax({
+  	        url: "https://kadai-reminder-server.herokuapp.com/upuser",
+  	        type: 'POST',
+            data :form.serialize(),
+  	        success: function(data) {
+  	            alert("Information Updated.");
+  	        }
+  	    });
+    });
+    });
+
   });
 
 });
